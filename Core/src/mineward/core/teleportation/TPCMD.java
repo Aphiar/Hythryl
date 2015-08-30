fad551056cc60084bc74e129b2dc5f8ec0dc17b8
InstanceOfDev <instanceofdev@gmail.com>
2015-08-30 17:13:40 +1000

    Allow teleporting 2 players to each other.

--- Core/src/mineward/core/teleportation/TPCMD.java
+++ Core/src/mineward/core/teleportation/TPCMD.java
@@ -4,6 +4,8 @@ import mineward.core.command.MyCommand;
 import mineward.core.common.Rank;
 import mineward.core.common.utils.C;
 import mineward.core.common.utils.F;
+import mineward.core.player.HPlayer;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
@@ -14,17 +16,31 @@ public class TPCMD extends MyCommand {
     }
 
     public void execute(Player p, String[] args) {
-        if (args.length == 0) {
-            F.help(p, "tp <player>", "Teleport to another player!", Rank.Jrmod);
+        if (args.length == 0 || args.length > 2) {
+            F.help(p, "tp <player> (player)", "Teleport to another player!", Rank.Jrmod);
             return;
         }
 
-        Player target = Bukkit.getPlayer(args[0]);
-        if (target != null) {
-            p.teleport(target);
-            F.message(p, "Teleport", "Teleported to " + C.STR_PLAYER + args[0]);
+        if (args.length == 1) {
+            Player target = Bukkit.getPlayer(args[0]);
+            if (target != null) {
+                p.teleport(target);
+                F.message(p, "Teleport", "Teleported to " + C.STR_PLAYER + args[0]);
+            } else {
+                F.message(p, "Teleport", "Player not online!");
+            }
+            return;
+        }
+
+        if (HPlayer.o(p).getRank().isPermissible(Rank.Admin)) {
+            Player player1 = Bukkit.getPlayer(args[0]);
+            Player player2 = Bukkit.getPlayer(args[1]);
+
+            player1.teleport(player2);
+            F.message(player1, "Teleport", C.STR_PLAYER + p.getName() + C.STR_MAIN + " teleported you to " + C.STR_PLAYER + player2.getName());
+            F.message(p, "Teleport", "You teleported " + C.STR_PLAYER + player1.getName() + C.STR_MAIN + " to " + C.STR_PLAYER + player2.getName());
         } else {
-            F.message(p, "Teleport", "Player not online!");
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&5Hierarchy&7] &7You need to be at least rank &c&lADMIN &7to do this."));
         }
     }
 }
