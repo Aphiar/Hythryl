964158d30b117dd86c135407b0deaaa02279d171
Zeh <zehrmc1@gmail.com>
2015-09-06 18:13:39 -0700

    FIX NPE

--- src/mineward/core/npc/NPCManager.java
+++ src/mineward/core/npc/NPCManager.java
@@ -10,7 +10,7 @@ public class NPCManager {
 	public static void Vegetate(Entity entity) {
 		net.minecraft.server.v1_8_R3.Entity e = ((CraftEntity) entity)
 				.getHandle();
-		NBTTagCompound tag = e.getNBTTag();
+		NBTTagCompound tag = new NBTTagCompound();
 		e.c(tag);
 		tag.setBoolean("NoAI", true);
 		e.f(tag);
